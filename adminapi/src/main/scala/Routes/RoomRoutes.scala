4577382bf046eeb1ed1626831ea3ace2c898b47c
ClementMounic <clement.mounic65@gmail.com>
2025-02-16 03:29:28 +0100

    modif deprecation + Future pour faire vrmt du scala

--- adminapi/src/main/scala/Routes/RoomRoutes.scala
+++ adminapi/src/main/scala/Routes/RoomRoutes.scala
@@ -5,15 +5,16 @@ import spray.json.RootJsonFormat
 import Models.Room
 import org.apache.pekko.http.scaladsl.server.Route
 import Repo.RoomDAO
-import org.apache.pekko.http.scaladsl.server.Directives._
-import org.apache.pekko.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import org.apache.pekko.http.scaladsl.server.Directives.*
+import org.apache.pekko.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
 import org.apache.pekko.http.scaladsl.model.StatusCodes
+import scala.util.{Success, Failure}
 
 
 
 trait RoomJsonFormats extends DefaultJsonProtocol{
-    implicit val roomFormat: RootJsonFormat[Room] = jsonFormat3(Room.apply)
-    implicit val roomListFormat: RootJsonFormat[List[Room]] = listFormat(roomFormat)
+    given roomFormat: RootJsonFormat[Room] = jsonFormat3(Room.apply)
+    given roomListFormat: RootJsonFormat[List[Room]] = listFormat(roomFormat)
 
 }
 
@@ -24,10 +25,13 @@ object RoomRoutes extends RoomJsonFormats{
             post{
                 entity(as[Room]){ room =>
                     val roomWithIdServer = room.copy(id_server = Some(id_server))
-                    if(RoomDAO.insertRoom(roomWithIdServer)){
-                        complete(StatusCodes.Created -> "Room added successfully")
-                    }else{
-                        complete(StatusCodes.InternalServerError -> "Error while inserting the room")
+                    onComplete(RoomDAO.insertRoom(roomWithIdServer)){
+                      case Success(true) =>  complete(StatusCodes.Created -> "Room added successfully")
+                    
+                      case Success(false) => complete(StatusCodes.InternalServerError -> "Error while inserting the room in the db")
+
+                      case Failure(ex) => complete(StatusCodes.InternalServerError -> s"An error occurred: ${ex.getMessage}")
+
                     }
                     
                 }
@@ -35,32 +39,36 @@ object RoomRoutes extends RoomJsonFormats{
             put{
             entity(as[Room]){ room =>
                 val roomWithIdServer = room.copy(id_server = Some(id_server))
-                if(RoomDAO.updateRoom(roomWithIdServer)){
-                    complete(StatusCodes.OK -> "Room updated successfully")
-                }else{
-                    complete(StatusCodes.InternalServerError -> "Error while updating the room")
-                }
+                onComplete(RoomDAO.updateRoom(roomWithIdServer)){
+                 case Success(true) =>   complete(StatusCodes.OK -> "Room updated successfully")
+                
+                 case Success(false) =>  complete(StatusCodes.InternalServerError -> "Error while updating the room in the db")
+                 
+                case Failure(ex) => complete(StatusCodes.InternalServerError -> s"An error occurred: ${ex.getMessage}")
 
             }
         } ~
             delete{
             entity(as[Room]){ room =>
                 val roomWithIdServer = room.copy(id_server = Some(id_server))
-                if(RoomDAO.deleteRoom(roomWithIdServer)){
-                    complete(StatusCodes.OK -> "Room deleted successfully")
+                onComplete(RoomDAO.deleteRoom(roomWithIdServer)){
+                 case Success(true) =>   complete(StatusCodes.OK -> "Room deleted successfully")
 
-                }else{
-                    complete(StatusCodes.InternalServerError -> "Error while deleting the room")
+                
+                 case Success(false) =>  complete(StatusCodes.InternalServerError -> "Error while deleting the room in the db")
 
-                }
+                 case Failure(ex) => complete(StatusCodes.InternalServerError -> s"An error occurred: ${ex.getMessage}")
+
 
             }
         } ~
             get{
+            
             complete(RoomDAO.getAllRoom(id_server))
         }
     
 } 
         }
-
-}
+    }
+        }
+    }
--- adminapi/src/main/scala/Routes/UserRoutes.scala
+++ adminapi/src/main/scala/Routes/UserRoutes.scala
@@ -2,20 +2,22 @@ package routes
 
 import models.User
 import dao.UserDAO
-import org.apache.pekko.http.scaladsl.server.Directives._
+import org.apache.pekko.http.scaladsl.server.Directives.*
 import org.apache.pekko.http.scaladsl.model.StatusCodes
 import org.apache.pekko.http.scaladsl.server.Route
-import spray.json._
-import org.apache.pekko.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.*
+import org.apache.pekko.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
 import Repo.UserServerDAO
-import org.apache.pekko.actor._
-import org.apache.pekko.stream.scaladsl._
-import org.apache.pekko.http.scaladsl._
-import org.apache.pekko.http.cors.scaladsl.CorsDirectives._
+import org.apache.pekko.actor.*
+import org.apache.pekko.stream.scaladsl.*
+import org.apache.pekko.http.scaladsl.*
+import org.apache.pekko.http.cors.scaladsl.CorsDirectives.*
+import scala.util.{Success, Failure}
+
 
 trait UserJsonFormats extends DefaultJsonProtocol {
-  implicit val userFormat: RootJsonFormat[User] = jsonFormat4(User.apply)
-  implicit val userListFormat: RootJsonFormat[List[User]] = listFormat(userFormat)
+  given userFormat: RootJsonFormat[User] = jsonFormat4(User.apply)
+  given userListFormat: RootJsonFormat[List[User]] = listFormat(userFormat)
 }
 
 object UserRoutes extends UserJsonFormats {
@@ -24,10 +26,13 @@ object UserRoutes extends UserJsonFormats {
       path("users") {
         post {
           entity(as[User]) { user =>
-            if (UserDAO.insertUser(user)) {
-              complete(StatusCodes.Created -> "User inserted successfully")
-            } else {
-              complete(StatusCodes.InternalServerError -> "Error inserting user")
+            onComplete(UserDAO.insertUser(user)) {
+              case Success(true) => complete(StatusCodes.Created -> "User inserted successfully")
+            
+             case Success(false) => complete(StatusCodes.InternalServerError -> "Error inserting user")
+
+            case Failure(ex) => complete(StatusCodes.InternalServerError -> s"An error occurred: ${ex.getMessage}")
+
             }
           }
         } ~
@@ -37,24 +42,30 @@ object UserRoutes extends UserJsonFormats {
       } ~
       path("user" / Segment) { username =>
         get {
-          UserDAO.getUserByUsername(username) match {
-            case Some(user) => complete(StatusCodes.OK, user)
-            case None => complete(StatusCodes.NotFound, s"User with username $username not found")
+          onComplete(UserDAO.getUserByUsername(username))  {
+            case Success(Some(user)) => complete(StatusCodes.OK, user)
+            case Success(None) => complete(StatusCodes.NotFound, s"User with username $username not found")
+            case Failure(ex) => complete(StatusCodes.InternalServerError -> s"An error occurred: ${ex.getMessage}")
+
           }
         } ~
         delete {
-          if (UserDAO.deleteUser(username)) {
-            complete(StatusCodes.OK -> s"User $username deleted successfully")
-          } else {
-            complete(StatusCodes.NotFound -> s"User $username not found or already deleted")
+          onComplete(UserDAO.deleteUser(username)) {
+            case Success(true) =>complete(StatusCodes.OK -> s"User $username deleted successfully")
+            case Success(false) => complete(StatusCodes.NotFound -> s"User $username not found or already deleted")
+           case Failure(ex) => complete(StatusCodes.InternalServerError -> s"An error occurred: ${ex.getMessage}")
+
           }
         } ~
         put {
           entity(as[User]) { user =>
-            if (UserDAO.updateUser(username, user.username, user.password)) {
-              complete(StatusCodes.OK -> s"User $username updated successfully")
-            } else {
-              complete(StatusCodes.NotFound -> s"User $username not found")
+            onComplete(UserDAO.updateUser(username, user.username, user.password)) {
+              case Success(true) => complete(StatusCodes.OK -> s"User $username updated successfully")
+          
+              case Success(false) =>complete(StatusCodes.NotFound -> s"User $username not found")
+
+              case Failure(ex) => complete(StatusCodes.InternalServerError -> s"An error occurred: ${ex.getMessage}")
+
             }
           }
         }
