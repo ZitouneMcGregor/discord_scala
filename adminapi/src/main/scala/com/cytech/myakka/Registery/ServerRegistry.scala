01e0cc39e26806153593e9561c87aee929b2136a
blj64 <jliogerbun@gmail.com>
2025-03-04 11:15:03 +0100

    Server Back

---
+++ adminapi/src/main/scala/com/cytech/myakka/Registery/ServerRegistry.scala
@@ -0,0 +1,88 @@
+package com.cytech.myakka.registry
+
+import org.apache.pekko.actor.typed.ActorRef
+import org.apache.pekko.actor.typed.Behavior
+import org.apache.pekko.actor.typed.scaladsl.Behaviors
+
+import scala.collection.immutable
+import doobie.*
+import doobie.implicits.*
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import com.cytech.myakka.configuration.PostgresConfig
+import doobie.postgres.sqlstate
+
+final case class Server(id: Option[Int], name: String, img: Option[String])
+final case class Servers(servers: immutable.Seq[Server])
+
+object ServerRegistry {
+  sealed trait Command
+  final case class GetServers(replyTo: ActorRef[Servers]) extends Command
+  final case class CreateServer(server: Server, replyTo: ActorRef[ActionPerformed]) extends Command
+  final case class GetServer(id: Int, replyTo: ActorRef[GetServerResponse]) extends Command
+  final case class DeleteServer(id: Int, replyTo: ActorRef[ActionPerformed]) extends Command
+  
+  final case class GetServerResponse(maybeServer: Option[Server])
+  final case class ActionPerformed(success: Boolean, description: String)
+
+  def apply(pgConfig: PostgresConfig): Behavior[Command] = {
+    registry(transactor(pgConfig))
+  }
+
+  type Transactor = doobie.Transactor.Aux[IO, Unit]
+  def transactor(pgConfig: PostgresConfig): Transactor = {
+    Transactor.fromDriverManager[IO](
+      driver = "org.postgresql.Driver",
+      url = pgConfig.url,
+      user = pgConfig.user,
+      password = pgConfig.pass,
+      logHandler = None
+    )
+  }
+
+  def dbGetServers(xa: Transactor): Servers = {
+    Servers(
+      sql"SELECT * FROM servers".
+      query[Server].
+      to[List].
+      transact(xa).
+      unsafeRunSync()
+    )
+  }
+
+  def dbGetServer(xa: Transactor, id: Int): Option[Server] = {
+    sql"SELECT * FROM servers WHERE id = $id".
+      query[Server].
+      option.
+      transact(xa).
+      unsafeRunSync()
+  }
+
+  def dbCreateServer(xa: Transactor, server: Server): IO[Either[String, Server]] = {
+    sql"INSERT INTO servers (name, img) VALUES (${server.name}, ${server.img})"
+      .update
+      .withUniqueGeneratedKeys[Server]("id", "name", "img")
+      .attemptSomeSqlState {
+        case sqlstate.class23.UNIQUE_VIOLATION => s"Error: Server name '${server.name}' already exists."
+      }
+      .transact(xa)
+  }
+
+  def dbDeleteServer(xa: Transactor, id: Int): Unit = {
+    sql"DELETE FROM servers WHERE id = $id".
+      update.
+      run.
+      transact(xa).
+      unsafeRunSync()
+  }
+
+  private def registry(xa: Transactor): Behavior[Command] =
+    Behaviors.receiveMessage {
+        case GetServers(replyTo) => 
+            replyTo ! dbGetServers(xa)
+            Behaviors.same
+        case _ => 
+            println("Received an unknown command.")
+            Behaviors.same
+    }
+}
---
+++ adminapi/src/main/scala/com/cytech/myakka/Routes/ServerRoutes.scala
@@ -0,0 +1,60 @@
+
+package com.cytech.myakka.routes
+
+import org.apache.pekko.http.scaladsl.server.Directives._
+import org.apache.pekko.http.scaladsl.model.StatusCodes
+import org.apache.pekko.http.scaladsl.server.Route
+
+import scala.concurrent.Future
+import com.cytech.myakka.registry.*
+import com.cytech.myakka.registry.ServerRegistry.*
+import org.apache.pekko.actor.typed.ActorRef
+import org.apache.pekko.actor.typed.ActorSystem
+import org.apache.pekko.http.scaladsl.server.directives.Credentials
+import org.apache.pekko.actor.typed.scaladsl.AskPattern._
+import org.apache.pekko.util.Timeout
+import org.apache.pekko.http.cors.scaladsl.CorsDirectives._
+import spray.json.*
+import spray.json.DefaultJsonProtocol._
+import com.cytech.myakka.configuration.BasicAuthConfig
+
+
+
+trait ServerJsonFormats extends DefaultJsonProtocol {
+  given serverFormat: RootJsonFormat[Server] = jsonFormat3(Server.apply)
+  given serverListFormat: RootJsonFormat[Servers] = jsonFormat1(Servers.apply)
+  given actionPerformedFormat: RootJsonFormat[ActionPerformed] = jsonFormat2(ActionPerformed.apply)
+  
+}
+
+
+class ServerRoutes(serverRegistry: ActorRef[ServerRegistry.Command], auth: BasicAuthConfig)(implicit val system: ActorSystem[_]) extends ServerJsonFormats, UserJsonFormats {
+  private given Timeout = Timeout.create(system.settings.config.getDuration("app.routes.ask-timeout"))
+
+  def getServers(): Future[Servers] = 
+    serverRegistry.ask(GetServers.apply)
+  
+  def myUserPassAuthenticator(credentials: Credentials): Option[String] = {
+    credentials match {
+      case p @ Credentials.Provided(id) if id == auth.user && p.verify(auth.password) => Some(id)
+      case _ => None
+    }
+   }
+
+  val serverRoutes: Route = cors() {
+    pathPrefix("servers") {
+         authenticateBasic(realm = "secure  site", myUserPassAuthenticator) { _ =>
+            concat(
+                pathEnd {
+                    concat(
+                        get {
+                            complete(getServers())
+                        },
+                    )
+                }
+            )
+         }
+    }
+  }
+}
+ 
